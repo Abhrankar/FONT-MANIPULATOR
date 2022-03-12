14cba1c4f7cb50837d6e523e17df197b40d2005d
Abhrankar <84310569+Abhrankar@users.noreply.github.com>
2022-03-12 10:09:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(400, 400);
@@ -12,6 +16,10 @@ function setup() {
 
 function draw() {
     background("#5196e3");
+
+    textSize(difference);
+    fill('#FFE787');
+    text('Peter', 50, 400);
 }
 
 function modelDone() {
@@ -25,7 +33,8 @@ function gotposes(results, error) {
     if (results.length > 0) {
         console.log(results);
 
-        console.log("rightWrist_x = " + results[0].pose.rightWrist.x + " rightWrist_y = " + results[0].pose.rightWrist.y);
-        console.log("leftWrist_x = " + results[0].pose.leftWrist.x + " leftWrist_y = " + results[0].pose.leftWrist.y);
+        leftWristX = results[0].pose.leftWrist.x;
+        righttWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
     }
 }
